5080c6e3bc81eee6000ac0f587a361b70f8a3e54
Hoodgail <45743294+Hoodgail@users.noreply.github.com>
2021-06-23 19:42:54 -0400

    fixed stylesheets for pathname hosting

--- docs/src/index.ts
+++ docs/src/index.ts
@@ -1,14 +1,14 @@
 import { stylesheet } from "./global";
 import Root from "./interface/Root";
 
-stylesheet("/stylesheet/Taskbar.css")
-stylesheet("/stylesheet/Root.css")
-stylesheet("/stylesheet/Desktop.css")
-stylesheet("/stylesheet/Item.css")
-stylesheet("/stylesheet/ContextMenu.css")
-stylesheet("/stylesheet/material-icons/material-icons.css")
-stylesheet("/stylesheet/DraggableWindow.css");
-stylesheet("/stylesheet/Toolbar.css")
+stylesheet("./stylesheet/Taskbar.css")
+stylesheet("./stylesheet/Root.css")
+stylesheet("./stylesheet/Desktop.css")
+stylesheet("./stylesheet/Item.css")
+stylesheet("./stylesheet/ContextMenu.css")
+stylesheet("./stylesheet/material-icons/material-icons.css")
+stylesheet("./stylesheet/DraggableWindow.css");
+stylesheet("./stylesheet/Toolbar.css")
 
 export const root = new Root();
 
